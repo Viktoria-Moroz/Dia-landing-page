0e2e814c0e3b63daf9150e47942edbb804152a9c
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2024-10-02 12:52:44 +0300

    make build

---
+++ index.1071f164.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("menu--with-menu"):document.body.classList.remove("menu--with-menu")}),document.querySelector(".contact-us__form").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});
+//# sourceMappingURL=index.1071f164.js.map
